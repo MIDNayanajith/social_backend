d1acf94bbf94207d8682e07d01fc749fe97ac65f
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-03-25 23:45:08 +0530

    develop register,login,logout functionality

--- Api/index.js
+++ Api/index.js
@@ -4,9 +4,21 @@ import authRoutes from "./routes/auth.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
 import postRoutes from "./routes/posts.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
